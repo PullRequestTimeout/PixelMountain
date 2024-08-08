7f6428f77ca1b180221c964e49632b88e7df8fa7
Jacob Druery <95809005+PullRequestTimeout@users.noreply.github.com>
2024-08-07 20:08:49 -0700

    :tada: Under construction page for Pixel Mountain website

---
+++ src/lib/components/Logo.svelte
@@ -0,0 +1,112 @@
+<div class="logo">
+	<div class="pixel-container">
+		<div class="logo-pixel"></div>
+		<div class="logo-pixel"></div>
+		<div class="logo-pixel"></div>
+		<div class="logo-pixel"></div>
+		<div class="logo-pixel"></div>
+		<div class="logo-pixel"></div>
+		<div class="logo-pixel"></div>
+		<div class="logo-pixel"></div>
+		<div class="logo-pixel"></div>
+		<div class="logo-pixel"></div>
+		<div class="logo-pixel"></div>
+		<div class="logo-pixel"></div>
+	</div>
+	<h1>PixelMountain<span>.</span></h1>
+</div>
+
+<style>
+	.logo {
+		display: flex;
+		align-items: center;
+		scale: 1;
+	}
+
+	@media (min-width: 768px) {
+		.logo {
+			scale: 2;
+		}
+	}
+
+	div.pixel-container {
+		position: relative;
+		translate: 0 -0.3rem;
+		scale: 0.75;
+	}
+
+	.logo-pixel {
+		width: 13px;
+		height: 13px;
+		background-color: #fff;
+		border-radius: 2px;
+		position: absolute;
+		left: 1.25rem;
+		scale: 0.75;
+	}
+
+	.logo-pixel:nth-child(1) {
+		translate: -1rem 0;
+	}
+
+	.logo-pixel:nth-child(2) {
+		translate: -8px -11px;
+	}
+
+	.logo-pixel:nth-child(3) {
+		translate: 0px -22px;
+	}
+
+	.logo-pixel:nth-child(4) {
+		translate: 7px -33px;
+	}
+
+	.logo-pixel:nth-child(5) {
+		translate: 14px -22px;
+	}
+
+	.logo-pixel:nth-child(6) {
+		translate: 21px -11px;
+	}
+
+	.logo-pixel:nth-child(7) {
+		translate: 14px 22px;
+	}
+
+	.logo-pixel:nth-child(8) {
+		translate: 21px 11px;
+	}
+
+	.logo-pixel:nth-child(9) {
+		translate: 28px 0;
+	}
+
+	.logo-pixel:nth-child(10) {
+		translate: 35px -11px;
+	}
+
+	.logo-pixel:nth-child(11) {
+		translate: 43px 0;
+	}
+
+	.logo-pixel:nth-child(12) {
+		translate: 50px 11px;
+	}
+
+	h1 {
+		font-family: "Josefin Sans";
+		font-weight: bold;
+		letter-spacing: -5px;
+		font-size: 3rem;
+		margin-left: 3.7rem;
+	}
+
+	.logo {
+		color: #fff;
+		mix-blend-mode: difference;
+	}
+
+	span {
+		margin-left: 2px;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,190 @@
+<script>
+	import Logo from "$lib/components/Logo.svelte";
+</script>
+
+<svelte:head>
+	<title
+		>Pixel Mountain Creative - Boutique digital agency nestled in the mountains of Beautiful British
+		Columbia</title
+	>
+</svelte:head>
+
+<main>
+	<p class="coming-soon">COMING SOON</p>
+	<div class="logo">
+		<Logo />
+	</div>
+	<p class="blurb">
+		Boutique digital agency nestled in the mountains of Beautiful British Columbia.
+	</p>
+	<form action="https://www.formbackend.com/f/9529c04929f3acd1" method="post">
+		<label for="name-input">Name:<span class="required">*</span></label>
+		<input type="text" name="name" id="name-input" placeholder="John Doe" required />
+		<label for="email-input">Email:<span class="required">*</span></label>
+		<input type="email" name="email" id="email-input" placeholder="johndoe@email.com" required />
+		<label for="subject-input">Subject:<span class="required">*</span></label>
+		<input
+			type="text"
+			name="subject"
+			id="subject-input"
+			placeholder="Project inquiry..."
+			required
+		/>
+		<label for="message-input">Message:<span class="required">*</span></label>
+		<textarea id="message-input" name="message" placeholder="Your message here..." required
+		></textarea>
+		<div class="button-container">
+			<button type="submit"
+				>Get In Touch <svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="24"
+					height="24"
+					viewBox="0 0 24 24"
+					fill="none"
+					stroke="currentColor"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					class="feather feather-arrow-right"
+					><line x1="5" y1="12" x2="19" y2="12"></line><polyline points="12 5 19 12 12 19"
+					></polyline></svg
+				></button
+			>
+		</div>
+	</form>
+</main>
+<video muted autoplay loop>
+	<source
+		src="https://tactusmarketing.com/wp-content/uploads/tactus-waves-hero.mp4"
+		type="video/mp4"
+	/>
+</video>
+
+<style>
+	main {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		height: 100vh;
+		font-family: sans-serif;
+		overflow: hidden;
+	}
+
+	p.coming-soon {
+		font-weight: 700;
+		margin-bottom: 0rem;
+		color: white;
+		margin-bottom: -1rem;
+	}
+
+	p.blurb {
+		color: white;
+		text-align: center;
+		font-style: italic;
+		margin-top: -2rem;
+		max-width: 15rem;
+		font-weight: 100;
+	}
+
+	@media (min-width: 768px) {
+		p.coming-soon {
+			margin-bottom: 1rem;
+		}
+
+		p.blurb {
+			margin-top: 0rem;
+		}
+	}
+
+	video {
+		position: fixed;
+		top: -10rem;
+		left: 0;
+		width: 100%;
+		height: calc(100vh + 10rem);
+		object-fit: cover;
+		z-index: -1;
+	}
+
+	form {
+		display: flex;
+		flex-direction: column;
+		background-color: #151515c9;
+		padding: 1.5rem;
+		border-radius: 1rem;
+		color: white;
+		width: 20rem;
+		max-width: calc(100vw - 5rem);
+		backdrop-filter: blur(2px);
+	}
+
+	@media (min-width: 768px) {
+		form {
+			padding: 2rem;
+		}
+	}
+
+	form label {
+		margin-bottom: 0.25rem;
+	}
+
+	form input,
+	form textarea {
+		border-radius: 0.5rem;
+		margin-bottom: 0.5rem;
+		font-family: sans-serif;
+		font-size: 0.9rem;
+		padding: 0.5rem;
+	}
+
+	form input,
+	form textarea,
+	form button {
+		color: white;
+		border: 2px solid #fff;
+		background-color: transparent;
+	}
+
+	form input:hover,
+	form textarea:hover,
+	form button:hover {
+		outline: 1px solid white;
+	}
+	form input:focus-visible,
+	form textarea:focus-visible,
+	form button:focus-visible {
+		border-color: var(--color-primary);
+		outline: none;
+	}
+
+	form textarea {
+		resize: none;
+		height: 4rem;
+		margin-bottom: 1rem;
+	}
+
+	form .required {
+		color: var(--color-primary);
+	}
+
+	form .button-container {
+		display: flex;
+		justify-content: center;
+	}
+
+	form button {
+		width: fit-content;
+		border-radius: 2rem;
+		cursor: pointer;
+		display: flex;
+		align-items: center;
+		padding: 0.25rem 1rem;
+		font-weight: 700;
+	}
+
+	form button svg {
+		margin-left: 0.5rem;
+		height: 20px;
+	}
+</style>
