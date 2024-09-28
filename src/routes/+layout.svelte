b668fbb8d4a2dad8dd03e2a020b832228da286a8
Jacob Druery <95809005+PullRequestTimeout@users.noreply.github.com>
2024-09-28 16:50:17 -0700

    :construction: Added components to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,2 +1,15 @@
 <script lang="ts">
+	import Navbar from "$lib/components/Navbar.svelte";
+	import Footer from "$lib/components/Footer.svelte";
+	import BackToTop from "$lib/components/BackToTop.svelte";
+	import CookieBanner from "$lib/components/CookieBanner.svelte";
+	import Icon from "$lib/components/Icon.svelte";
 </script>
+
+<Navbar />
+<slot />
+<Footer />
+<BackToTop>
+	<Icon name="arrow-up" color="#fff" size="32" />
+</BackToTop>
+<CookieBanner />
