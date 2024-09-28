b0699d279d722d5a83a9ffed9833411e63ee41a3
Jacob Druery <95809005+PullRequestTimeout@users.noreply.github.com>
2024-09-28 16:43:45 -0700

    :bento: Added icons

--- src/lib/components/Icon.svelte
+++ src/lib/components/Icon.svelte
@@ -1,13 +1,10 @@
 <script lang="ts">
 	export let name = "";
-	export let color = "--clr-white";
+	export let color = "--color-white";
 	export let size = "24";
 
 	let inputColor;
 	switch (color) {
-		case "--clr-dark-red":
-			inputColor = "#a0393b";
-			break;
 		default:
 			inputColor = color;
 			break;
@@ -69,7 +66,19 @@
 		{
 			name: "help",
 			svg: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" width=${size} fill=${inputColor}><path d="M478-240q21 0 35.5-14.5T528-290q0-21-14.5-35.5T478-340q-21 0-35.5 14.5T428-290q0 21 14.5 35.5T478-240Zm-36-154h74q0-33 7.5-52t42.5-52q26-26 41-49.5t15-56.5q0-56-41-86t-97-30q-57 0-92.5 30T342-618l66 26q5-18 22.5-39t53.5-21q32 0 48 17.5t16 38.5q0 20-12 37.5T506-526q-44 39-54 59t-10 73Zm38 314q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>`
+		},
+		{
+			name: "arrow-right",
+			svg: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" width=${size} fill=${inputColor}><path d="M647-440H160v-80h487L423-744l57-56 320 320-320 320-57-56 224-224Z"/></svg>`
+		},
+		{
+			name: "arrow-up",
+			svg: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" width=${size} fill=${inputColor}><path d="M440-160v-487L216-423l-56-57 320-320 320 320-56 57-224-224v487h-80Z"/></svg>`
 		}
+		// {
+		// 	name: "",
+		// 	svg: ``
+		// }
 		// {
 		// 	name: "",
 		// 	svg: ``
