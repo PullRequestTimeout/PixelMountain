16b6d5ab31bb829022f389e9bd20e0141560987d
Jacob Druery <95809005+PullRequestTimeout@users.noreply.github.com>
2024-10-09 13:45:50 -0700

    :fire: Remove janky fixes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -22,39 +22,8 @@
 
 	// fix the lack of dynamic viewport height on ios firefox (pls firefox my beloved 😢)
 	let vh = 0;
-	// let isScrolling = false;
-	// let scrollTimeout: ReturnType<typeof setTimeout>;
-	// let isResizing = false;
-	// let resizeTimeout: ReturnType<typeof setTimeout>;
-
 	onMount(() => {
 		vh = window.innerHeight;
-
-		// window.addEventListener("scroll", () => {
-		// 	isScrolling = true;
-
-		// 	// Clear the timeout if the user is still scrolling
-		// 	clearTimeout(scrollTimeout);
-
-		// 	// Set a timeout to reset the scrolling state after 100ms of inactivity
-		// 	scrollTimeout = setTimeout(() => {
-		// 		isScrolling = false;
-		// 	}, 200);
-		// });
-
-		// window.addEventListener("resize", () => {
-		// 	isResizing = true;
-
-		// 	if (isResizing && !isScrolling) vh = window.innerHeight;
-
-		// 	// Clear the timeout if the user is still resizing
-		// 	clearTimeout(resizeTimeout);
-
-		// 	// Set a timeout to reset the resizing state after 100ms of inactivity
-		// 	resizeTimeout = setTimeout(() => {
-		// 		isResizing = false;
-		// 	}, 200);
-		// });
 	});
 </script>
 
@@ -67,16 +36,3 @@
 	<Icon name="arrow-up" color="#fff" size="32" />
 </BackToTop>
 <CookieBanner />
-<p>Viewheight: {vh}px</p>
-
-<style>
-	p {
-		position: fixed;
-		top: 0;
-		right: 0;
-		background: var(--color-black);
-		color: var(--color-white);
-		padding: 1rem;
-		z-index: 1000;
-	}
-</style>
