a30d6de6449a61162d6890261f7d71b77622e7a7
Jacob Druery <95809005+PullRequestTimeout@users.noreply.github.com>
2024-10-12 11:05:56 -0700

    :recycle: Changed scroll progress to calculate percentage of whole page, not blog height

--- src/lib/components/ScrollProgress.svelte
+++ src/lib/components/ScrollProgress.svelte
@@ -1,21 +1,12 @@
-<!--
-    Client height needs to be passed to the ScrollProgress component from the most top-level element in /routes/+page.svelte or /routes/+layout.svelte:
-    
-    <main bind:clientHeight>
-	    <ScrollProgress totalHeight={clientHeight} />
-    </main>
- -->
-
 <script>
-	export let totalHeight = 0;
 	let scrollPos = 0;
 	let innerHeight = 0;
-	$: scrollPercentage = (scrollPos / (totalHeight - innerHeight)) * 100;
+	$: scrollPercentage = (scrollPos / innerHeight) * 100;
 </script>
 
 <svelte:window bind:scrollY={scrollPos} bind:innerHeight />
 
-{#if scrollPos && totalHeight && innerHeight}
+{#if scrollPos && innerHeight}
 	<div class="scroll-pos-bar" style="--scroll-percent: {scrollPercentage}%">
 		<div class="scroll-pos-progress"></div>
 	</div>
@@ -37,7 +28,6 @@
 		width: 0;
 		width: var(--scroll-percent);
 		height: 100%;
-		background-color: rgb(0, 174, 255);
-		/* background-color: var(--clr-primary); */
+		background-color: var(--color-primary);
 	}
 </style>
